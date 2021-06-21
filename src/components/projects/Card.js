eb547cedd3bbab3ff18405334a2b3ac542677859
Michelle Chen <michelle800902@gmail.com>
2021-06-21 23:01:19 +0800

    Add component card in projects

---
+++ src/components/projects/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+`;
+
+function Card(props) {
+    return (
+        <CardWrapper>
+            {}
+        </CardWrapper>
+    );
+}
+
+export default Card;
