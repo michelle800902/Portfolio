d567a3b6558648b690b475103c2c014524585dc6
Michelle Chen <michelle800902@gmail.com>
2022-01-09 03:24:15 +0800

    Add icon resize eventlistener and update title font size for experience

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import device from '../../assets/responsive/breakpoints';
 import { experienceData } from '../../constants/index';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 const ExperienceWrapper = styled.div`
     width: 100%;
-    height: 200vh;
+    padding: 5% 0;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
 `;
 const Title = styled.div`
+    font-size: 40px;
     font-family: 'AvenirHeavy';
-    font-size: 72px;
     color: var(--title-color);
+    @media ${device.mobileL} {
+        font-size: 50px;
+    }
+    @media ${device.tablet} {
+        font-size: 60px;
+    }
+    @media ${device.laptop} {
+        font-size: 70px;
+    }
+    @media ${device.laptopL} {
+        font-size: 80px;
+    }
+    @media ${device.desktop} {
+        font-size: 100px;
+    }
 `;
 const CompanyIcon = styled.div`
-    width: 60px;
-    height: 60px;
+    width: ${props => props.width}px;
+    height: ${props => props.height}px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -27,18 +43,53 @@ const CompanyIcon = styled.div`
     background-color: var(--white);
 `;
 const IconImg = styled.img`
-    width: 40px;
-    height: 40px;
+    width: ${props => props.width}px;
+    height: ${props => props.height}px;
 `;
 
 function Experience() {
+    const [iconWidth, setIconWidth] = useState(0);
+    const [iconHeight, setIconHeight] = useState(0);
+
+    const updateIconSize = () => {
+        const iconElements = document.getElementsByClassName('vertical-timeline-element-icon');
+        if (iconElements && iconElements.length) {
+            const iconElementWidth = iconElements[0].offsetWidth;
+            const iconElementHeight = iconElements[0].offsetHeight;
+            if (iconWidth !== iconElementWidth) setIconWidth(iconElementWidth);
+            if (iconHeight !== iconElementHeight) setIconHeight(iconElementHeight);
+        }
+    };
+
+    useEffect(() => {
+        updateIconSize();
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('resize', updateIconSize)
+        return () => {
+            window.removeEventListener('resize', updateIconSize);
+        }
+    })
+
     const renderTimelineElement = (data, i) => {
         return (
             <VerticalTimelineElement
                 key={i}
                 className="vertical-timeline-element--work"
                 date={data.date}
-                icon={<CompanyIcon><IconImg src={data.icon} /></CompanyIcon>}
+                icon={(
+                    <CompanyIcon
+                        width={iconWidth}
+                        height={iconHeight}
+                    >
+                        <IconImg
+                            width={iconWidth - 12}
+                            height={iconHeight - 12}
+                            src={data.icon}
+                        />
+                    </CompanyIcon>
+                )}
                 intersectionObserverProps={{ rootMargin: '0px 0px 40px 0px' }}
             >
                 <h3 className="vertical-timeline-element-title">{data.title}</h3>
